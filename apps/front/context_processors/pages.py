6c38063a4e4f001391617784a780f44eb01e6afe
Kobe Cuppens <kobecuppens@hotmail.com>
2021-05-07 15:51:56 +0200

    current page update

--- apps/front/context_processors/pages.py
+++ apps/front/context_processors/pages.py
@@ -1,8 +1,11 @@
 from apps.pages.models import Page
 def get_menu(request):
+    slug = request.resolver_match.kwargs.get('slug')
+    current_page = Page.objects.filter(full_slug=slug).first()
     return {
         'pages': Page.objects.filter(active=True, date_deleted=None, in_main_menu=True),
         'topnav': Page.objects.filter(active=True, date_deleted=None, in_topnav_menu=True),
-        'quicklinks': Page.objects.filter(active=True, date_deleted=None, in_quicklinks_menu=True)
+        'quicklinks': Page.objects.filter(active=True, date_deleted=None, in_quicklinks_menu=True),
+        'current_page': current_page
     }
 
